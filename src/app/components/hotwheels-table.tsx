251c437c2c09e956494287838f2fa7706aefbd3a
Ashwin Cherian Joseph <iamashwincherian@gmail.com>
2024-12-06 12:01:37 +0530

    add loader

--- src/app/components/hotwheels-table.tsx
+++ src/app/components/hotwheels-table.tsx
@@ -5,7 +5,6 @@ import { Loader2, CloudUpload } from "lucide-react";
 
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
 import {
   Table,
   TableBody,
@@ -21,22 +20,34 @@ import updateHotwheels from "@/server/update-hotwheels";
 import getCurrentProducts from "@/server/get-current-products";
 import { NewDropsDialog } from "./new-drops-dialog";
 
+const Loading = () => {
+  return (
+    <div className="flex gap-2 mx-0 my-4">
+      <Loader2 className="animate-spin h-6 w-6" />
+      Loading
+    </div>
+  );
+};
+
 export default function HotwheelsTable() {
-  const [isPending, startTransition] = useTransition();
+  const [isUpdatePending, startUpdate] = useTransition();
+  const [isFetchPending, startFetch] = useTransition();
   const [hotwheels, setHotwheels] = useState<string[]>([]);
   const [updatedOn, setUpdatedOn] = useState<Date | null>(null);
   const [openNewDropsDialog, setOpenNewDropsDialog] = useState(false);
   const [newDrops, setNewDrops] = useState<string[]>([]);
 
   useEffect(() => {
-    getCurrentProducts().then((data) => {
-      setHotwheels(data.products);
-      setUpdatedOn(data.updatedOn);
+    startFetch(() => {
+      getCurrentProducts().then((data) => {
+        setHotwheels(data.products);
+        setUpdatedOn(data.updatedOn);
+      });
     });
   }, []);
 
   const handleUpdate = () => {
-    startTransition(() => {
+    startUpdate(() => {
       updateHotwheels().then(({ products, newDrops, updatedOn }) => {
         setHotwheels(products);
         setNewDrops(newDrops);
@@ -59,7 +70,7 @@ export default function HotwheelsTable() {
   };
 
   return (
-    <div className="max-w-[600px] mx-auto">
+    <div className="flex items-center flex-col max-w-[600px] mx-auto">
       <NewDropsDialog
         open={openNewDropsDialog}
         newDrops={newDrops}
@@ -83,40 +94,52 @@ export default function HotwheelsTable() {
           </AlertDescription>
         </div>
         <div>
-          <Button variant="outline" disabled={isPending} onClick={handleUpdate}>
-            {isPending ? <Loader2 className="animate-spin" /> : <CloudUpload />}
+          <Button
+            variant="outline"
+            disabled={isUpdatePending}
+            onClick={handleUpdate}
+          >
+            {isUpdatePending ? (
+              <Loader2 className="animate-spin" />
+            ) : (
+              <CloudUpload />
+            )}
             Update List
           </Button>
         </div>
       </Alert>
-      <Table className="border">
-        <TableCaption>
-          Data from{" "}
-          <a href="https://karzanddolls.com/" target="_blank">
-            Karzanddolls.com
-          </a>
-        </TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Name</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {hotwheels.map((car) => (
-            <TableRow key={car}>
-              <TableCell>{car}</TableCell>
+      {isFetchPending ? (
+        <Loading />
+      ) : (
+        <Table className="border">
+          <TableCaption>
+            Data from{" "}
+            <a href="https://karzanddolls.com/" target="_blank">
+              Karzanddolls.com
+            </a>
+          </TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Name</TableHead>
             </TableRow>
-          ))}
-        </TableBody>
-        <TableFooter>
-          <TableRow>
-            <TableCell className="flex justify-between">
-              Total
-              <p className="text-right">{hotwheels.length}</p>
-            </TableCell>
-          </TableRow>
-        </TableFooter>
-      </Table>
+          </TableHeader>
+          <TableBody>
+            {hotwheels.map((car) => (
+              <TableRow key={car}>
+                <TableCell>{car}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TableCell className="flex justify-between">
+                Total
+                <p className="text-right">{hotwheels.length}</p>
+              </TableCell>
+            </TableRow>
+          </TableFooter>
+        </Table>
+      )}
     </div>
   );
 }

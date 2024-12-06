0b70057e82d7d326e587218c8fe234132af32e9f
Ashwin Cherian Joseph <iamashwincherian@gmail.com>
2024-12-06 10:28:15 +0530

    fix save

--- src/server/save-products.ts
+++ src/server/save-products.ts
@@ -8,7 +8,7 @@ export default async function saveProducts(hotwheels: string[]) {
 
   const { blobs: cloudBlobs } = await list();
   const file = cloudBlobs.find((file) => file.pathname === "data.json");
-  // if (file) del(file.downloadUrl);
-  // await put("data.json", blob, { access: "public" });
+  if (file) del(file.downloadUrl);
+  await put("data.json", blob, { access: "public" });
   return data;
 }

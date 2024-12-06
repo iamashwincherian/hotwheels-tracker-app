0b6d0dd4311628cdfc8d60f9c2ce3949435f3a24
Ashwin Cherian Joseph <iamashwincherian@gmail.com>
2024-12-06 09:31:30 +0530

    update data json path

--- src/server/get-current-products.ts
+++ src/server/get-current-products.ts
@@ -8,7 +8,7 @@ export default async function getCurrentProducts() {
   let updatedOn = null;
 
   try {
-    const file = await fs.readFile(process.cwd() + "/src/data.json", "utf8");
+    const file = await fs.readFile(process.cwd() + "data.json", "utf8");
     const data = JSON.parse(file);
     existingProducts = data.hotwheels;
     updatedOn = data.updatedOn && new Date(data.updatedOn);
--- src/server/save-products.ts
+++ src/server/save-products.ts
@@ -2,12 +2,10 @@
 
 import { promises as fs } from "fs";
 
-export default async function saveProducts(
-  hotwheels: string[],
-) {
+export default async function saveProducts(hotwheels: string[]) {
   const data = { hotwheels, updatedOn: new Date() };
   await fs.writeFile(
-    process.cwd() + "/src/data.json",
+    process.cwd() + "data.json",
     JSON.stringify(data, null, 2),
     "utf8"
   );
